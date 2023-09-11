3c90d89eeb9b7fdab4989b867b472b2835912656
Yana <youremail@yourdomain.com>
2023-09-11 16:51:05 +0200

    fin

--- src/Laboratoire.java
+++ src/Laboratoire.java
@@ -1,8 +1,16 @@
+import java.util.ArrayList;
+
 public class Laboratoire {
+
     public static void main(String[] args) throws Exception {
+        ArrayList<Souris> mesSouris;
+        mesSouris = new ArrayList<Souris>();
         Souris s1 = new Souris(50, "blanche", 2);
+        mesSouris.add(s1);
         Souris s2 = new Souris(45, "grise");
+        mesSouris.add(s2);
         Souris s3 = new Souris(s2);
+        mesSouris.add(s3);
         
         System.out.println(s1);
         System.out.println(s2);
@@ -10,7 +18,7 @@ public class Laboratoire {
         
         s1.vieillir();
         s1.vieillir();
-        
+
         System.out.println(s1);
         
         s1.evolue();
