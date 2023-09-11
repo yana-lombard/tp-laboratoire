c8d467e94520f12c17a64b63ff7d761c473eb22e
Yana <youremail@yourdomain.com>
2023-09-11 11:32:55 +0200

    first commit

---
+++ src/Laboratoire.java
@@ -0,0 +1,24 @@
+public class Laboratoire {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Souris s1 = new Souris(50, "blanche", 2);
+        Souris s2 = new Souris(45, "grise");
+        Souris s3 = new Souris(s2);
+        
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        
+        s1.vieillir();
+        s1.vieillir();
+        System.out.println(s1);
+        
+        s1.evolue();
+        s2.evolue();
+        s3.evolue();
+        
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+}
---
+++ src/Souris.java
@@ -0,0 +1,5 @@
+public class Souris {
+    public void evolue() {
+
+    }
+}
